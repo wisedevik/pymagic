456d634e1770874993bb40b8ab6f6811bf7e5660
anybody <>
2025-06-09 11:53:36 +0700

    Little refactors

--- server/database/__init__.py
+++ server/database/__init__.py
@@ -2,4 +2,3 @@ __all__ = ["AsyncSessionLocal", "AccountCrud", "init_db"]
 
 from server.database.base import AsyncSessionLocal, init_db
 from server.database.crud import AccountCrud
-
--- server/database/base.py
+++ server/database/base.py
@@ -9,6 +9,7 @@ engine = create_async_engine(
 )
 AsyncSessionLocal = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
 
+
 async def init_db():
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
